ead2264e3c7ec0d53d4f94d0215c9a380c39ea97
CamiloTello002 <camilojtello5@gmail.com>
2025-04-07 17:46:25 -0500

    feat: add validation to pokemon creation dto

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,10 @@ import { MongooseModule } from '@nestjs/mongoose';
 @Module({
   imports: [
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'public')
+      rootPath: join(__dirname, '..', 'public'),
     }),
     PokemonModule,
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon')
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
   ],
 })
 export class AppModule { }
