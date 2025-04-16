32aa44a427e7e26438a7be6e7b3ab645272fa502
CamiloTello002 <camilojtello5@gmail.com>
2025-04-16 15:27:06 -0500

    chore: register Pokemon model dependency

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Pokemon, PokemonSchema } from 'src/pokemon/entities/pokemon.entity';
 
 @Module({
   controllers: [SeedController],
   providers: [SeedService],
+  imports: [
+    // registers model with NestJS's dependency injection system
+    // Yes, this dependency needs to be registered
+    MongooseModule.forFeature([
+      {
+        name: Pokemon.name,
+        schema: PokemonSchema
+      }
+    ])
+  ]
 })
-export class SeedModule {}
+export class SeedModule { }
