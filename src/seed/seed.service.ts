419664ef939175f1570e24e93c00728aa8f9925b
CamiloTello002 <camilojtello5@gmail.com>
2025-04-16 15:27:23 -0500

    feat: seed DB with pokemons coming from PokeAPI

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,24 +1,46 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance, AxiosStatic } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class SeedService {
 
-  // remember that this is not a service, but a dependency
-  // By the way, you need to initialize the member
+  constructor(
+    // I want to inject the MongoDB model (yes, a model is a dependency)
+    // but... where does it come from?
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>
+  ) { }
+
   private readonly axios: AxiosStatic = axios;
 
   async executeSeed() {
-    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=1')
+    try {
+      const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10')
+      const pokemonList = data.results.map(({ name, url }) => {
+        const segments = url.split('/');
+        const no: number = +segments[segments.length - 2]
+        return { name, no }
+      })
 
-    data.results.forEach(({ name, url }) => {
-      const segments = url.split('/');
-      const no: number = +segments[segments.length - 2]
-      console.log({ name, no })
-    })
+      const operations = pokemonList.map(pokemon => ({
+        updateOne: {
+          filter: { no: pokemon.no },
+          update: { $set: pokemon },
+          upsert: true
+        },
+      }));
 
-    return data;
+      await this.pokemonModel.bulkWrite(operations);
+
+      return { message: 'Seed completed', count: operations.length };
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
   }
 
 }
