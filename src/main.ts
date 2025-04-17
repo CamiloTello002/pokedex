3841f333c3e2b0a9469824e766d6c54ffb57ad05
CamiloTello002 <camilojtello5@gmail.com>
2025-04-17 15:37:57 -0500

    feat: implement implicit type conversion

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,12 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
-      forbidNonWhitelisted: true
+      forbidNonWhitelisted: true,
+      // good for automatic type conversion, but consumes more memory
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true
+      }
     })
   )
 
