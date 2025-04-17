25b08a1e7e8cec37f5c199f186fc9318920d947c
CamiloTello002 <camilojtello5@gmail.com>
2025-04-17 14:38:42 -0500

    refactor: retrieve pokemons with http adapter

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -3,19 +3,19 @@ import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Pokemon, PokemonSchema } from 'src/pokemon/entities/pokemon.entity';
+import { CommonModule } from 'src/common/common.module';
 
 @Module({
   controllers: [SeedController],
   providers: [SeedService],
   imports: [
-    // registers model with NestJS's dependency injection system
-    // Yes, this dependency needs to be registered
     MongooseModule.forFeature([
       {
         name: Pokemon.name,
         schema: PokemonSchema
       }
-    ])
+    ]),
+    CommonModule
   ]
 })
 export class SeedModule { }
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -4,39 +4,32 @@ import { PokeResponse } from './interfaces/poke-response.interface';
 import { InjectModel } from '@nestjs/mongoose';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { Model } from 'mongoose';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
 
   constructor(
-    // I want to inject the MongoDB model (yes, a model is a dependency)
-    // but... where does it come from?
     @InjectModel(Pokemon.name)
-    private readonly pokemonModel: Model<Pokemon>
+    private readonly pokemonModel: Model<Pokemon>,
+    private readonly http: AxiosAdapter
   ) { }
 
-  private readonly axios: AxiosStatic = axios;
 
   async executeSeed() {
     try {
-      const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10')
+      await this.pokemonModel.deleteMany({});
+
+      const data = await this.http.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10')
       const pokemonList = data.results.map(({ name, url }) => {
         const segments = url.split('/');
         const no: number = +segments[segments.length - 2]
         return { name, no }
       })
 
-      const operations = pokemonList.map(pokemon => ({
-        updateOne: {
-          filter: { no: pokemon.no },
-          update: { $set: pokemon },
-          upsert: true
-        },
-      }));
+      await this.pokemonModel.insertMany(pokemonList);
 
-      await this.pokemonModel.bulkWrite(operations);
-
-      return { message: 'Seed completed', count: operations.length };
+      return { message: 'Seed completed', count: pokemonList.length };
     } catch (error) {
       console.log(error);
       throw error;
