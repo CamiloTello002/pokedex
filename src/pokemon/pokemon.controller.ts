ae57d02559507fc5c8d9e6f61e2fd3e2f59ed1e9
CamiloTello002 <camilojtello5@gmail.com>
2025-04-17 15:10:19 -0500

    feat: pass query params to service

--- src/pokemon/pokemon.controller.ts
+++ src/pokemon/pokemon.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { PokemonService } from './pokemon.service';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { ParseMongoIdPipe } from 'src/common/pipes/parse-mongo-id/parse-mongo-id.pipe';
+import { PaginationQueryDto } from './dto/pagination-query.dto';
 
 @Controller('pokemon')
 export class PokemonController {
@@ -14,8 +15,8 @@ export class PokemonController {
   }
 
   @Get()
-  findAll() {
-    return this.pokemonService.findAll();
+  findAll(@Query() paginationQuery: PaginationQueryDto) {
+    return this.pokemonService.findAll(paginationQuery);
   }
 
   @Get(':term')
